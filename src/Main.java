dae30b8b451e7541617406f7f9e3bca0b58e450f
Vladimir Protcay <protcay2@mail.ru>
2021-12-24 02:49:38 +0300

    исправлено

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ public class Main {
 
             int balance = 100;
             int deposit = 2000;
-            int bonus = (deposit + balance) / 100;
-            int overallBalance = balance + deposit + bonus;
+            int bonus = 0;
+            int overallBalance = balance + deposit + bonus ;
             if (deposit >= 1000) {
                 bonus = (deposit + balance) / 100;
             } else {
                 bonus = 0;
             }
-            System.out.println(overallBalance);
+            System.out.println("Ваш баланс:"+ overallBalance);
+            System.out.println("Ваши начисленные бонусы:"+ bonus);
         }
     }
 
